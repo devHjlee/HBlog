82423e776975de756973433fb3c11b6fcfd862a5
leehyungjae <dlgudwo11@gmail.com>
2020-02-29 14:54:41 +0900

    Spring boot proejct 생성

---
+++ src/main/java/com/hblog/HlbogApplication.java
@@ -0,0 +1,13 @@
+package com.hblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlbogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlbogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hblog/HlbogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlbogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
