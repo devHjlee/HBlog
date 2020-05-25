6ff8734c4183b20838a023fbc4fa14586eaa4418
LeeHyungJae <dlgudwo11@gmail.com>
2020-05-25 15:22:49 +0900

    Thymeleaf Template Add  - thymeleaf 사용을위한 dependency 추가 및 테스트용 index.html 추가

--- src/main/java/com/hblog/WebTestController.java
+++ src/main/java/com/hblog/WebTestController.java
@@ -1,14 +1,22 @@
 package com.hblog;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebTestController {
 
     @GetMapping("/")
-    public String webTest() {
-
-        return "Spring Boot Test";
+    public String webTest(Model model) {
+        Map<String,String> testVO = new HashMap<String,String>();
+        testVO.put("id","dlgudwo11");
+        testVO.put("name","이형재");
+        model.addAttribute("testModel", testVO);
+        model.addAttribute("message","Spring Boot Main");
+        return "index";
     }
 }
